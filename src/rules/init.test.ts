232bc318215b22b8245ae44115bddf6825f5c2e1
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2024-08-12 23:49:33 -0400

    feat(init): added init to the rules and a test for the init rule (#5)

---
+++ src/rules/init.test.ts
@@ -0,0 +1,33 @@
+import { assertEquals } from 'jsr:@std/assert'
+import { ruleInitFunc } from './init.ts'
+import { TestFileSystem } from '../util.test.ts'
+
+Deno.test('rule init should not create the file if it exists', async () => {
+  const rule = {
+    type: 'init',
+    path: 'path/to/file',
+    content: 'test',
+  } as const
+
+  const fs = TestFileSystem({
+    [rule.path]: 'content',
+  })
+
+  await ruleInitFunc(rule, fs)
+
+  assertEquals(await fs.read(rule.path), undefined)
+})
+
+Deno.test('rule init should create the file in the filesystem', async () => {
+  const fs = TestFileSystem()
+
+  const rule = {
+    type: 'init',
+    path: 'path/to/file',
+    content: 'test',
+  } as const
+
+  await ruleInitFunc(rule, fs)
+
+  assertEquals(await fs.read(rule.path), 'test')
+})
