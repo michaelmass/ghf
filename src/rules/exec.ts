e9bec0688ea66b7a69e6c6134216749329e999d6
michaelmass <10071388+michaelmass@users.noreply.github.com>
2024-09-22 22:52:23 -0400

    chore(exec): added exec rule

--- src/rules/exec.ts
+++ src/rules/exec.ts
@@ -1,1 +1,12 @@
-// TODO implement exec rules
+import { z } from '../deps.ts'
+import type { FileSystem } from '../filesystem.ts'
+
+export const ruleExecSchema = z.object({
+  type: z.literal('exec'),
+})
+
+type RuleExec = z.infer<typeof ruleExecSchema>
+
+export const ruleExecFunc = async (_: RuleExec, __: FileSystem) => {
+  // TODO implement ruleExecFunc
+}
