09373c91a88dcd1389c54e321ddefdc4888702ff
michaelmass <10071388+michaelmass@users.noreply.github.com>
2024-09-23 22:10:19 -0400

    chore(merge): added merge rule

--- src/rules/merge.ts
+++ src/rules/merge.ts
@@ -1,1 +1,12 @@
-// TODO implement merge rules
+import { z } from '../deps.ts'
+import type { FileSystem } from '../filesystem.ts'
+
+export const ruleMergeSchema = z.object({
+  type: z.literal('merge'),
+})
+
+type RuleMerge = z.infer<typeof ruleMergeSchema>
+
+export const ruleMergeFunc = async (_: RuleMerge, __: FileSystem) => {
+  // TODO implement ruleMergeFunc
+}
