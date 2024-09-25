0b7a52e1f0b256757b1fcc2143fe2c74d37a95e2
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2024-09-24 23:45:00 -0400

    chore(merge): updated merge rule (#27)

--- src/rules/merge.ts
+++ src/rules/merge.ts
@@ -1,8 +1,11 @@
 import { z } from '../deps.ts'
 import type { FileSystem } from '../filesystem.ts'
+import { contentSchema } from '../schemas.ts'
 
 export const ruleMergeSchema = z.object({
   type: z.literal('merge'),
+  path: z.string(),
+  content: contentSchema,
 })
 
 type RuleMerge = z.infer<typeof ruleMergeSchema>
