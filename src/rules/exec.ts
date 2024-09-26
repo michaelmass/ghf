e959e05d539bf3ac01cd3e99a79ef58c0fca714f
michaelmass <10071388+michaelmass@users.noreply.github.com>
2024-09-25 22:53:33 -0400

    chore(exec): updated exec rule

--- src/rules/exec.ts
+++ src/rules/exec.ts
@@ -3,6 +3,10 @@ import type { FileSystem } from '../filesystem.ts'
 
 export const ruleExecSchema = z.object({
   type: z.literal('exec'),
+  cmd: z.string(),
+  args: z.array(z.string()),
+  cwd: z.string(),
+  env: z.record(z.string()),
 })
 
 type RuleExec = z.infer<typeof ruleExecSchema>
