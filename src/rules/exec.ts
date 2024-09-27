f2d3ecf18cf64a5b9a967717eb84fc9d95da8a06
michaelmass <10071388+michaelmass@users.noreply.github.com>
2024-09-26 23:33:45 -0400

    chore(exec): updated exec rule

--- src/rules/exec.ts
+++ src/rules/exec.ts
@@ -11,6 +11,17 @@ export const ruleExecSchema = z.object({
 
 type RuleExec = z.infer<typeof ruleExecSchema>
 
-export const ruleExecFunc = async (_: RuleExec, __: FileSystem) => {
+export const ruleExecFunc = async ({ cmd, args, cwd, env }: RuleExec, __: FileSystem) => {
+  const command = new Deno.Command(cmd, {
+    args,
+    cwd,
+    env,
+  })
+
+  const output = await command.output()
+
+  if (output.code !== 0) {
+    throw new Error(`Command exited with code ${output.code}`)
+  }
   // TODO implement ruleExecFunc
 }
